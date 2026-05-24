7ca452c288dd7a19f3bc4fda26189bd201dd8776
Jzhenli <jzhenl@foxmail.com>
2026-05-25 07:33:06 +0800

    build

--- apps/xagent/src/xagent/resources/static/assets/index.esm.min-CCsB-rrI.js
+++ apps/xagent/src/xagent/resources/static/assets/index.esm.min-CCsB-rrI.js
@@ -1,4 +1,4 @@
-import{d as Qy,af as kf,a0 as Wn,Z as Jy,C as Un,ak as Of,q as jy,a4 as tm,a6 as Yn,aa as em,a3 as rm,z as Zr,a9 as im,v as nm}from"./index-DSv8PiCG.js";/*! *****************************************************************************
+import{d as Qy,af as kf,a0 as Wn,Z as Jy,C as Un,ak as Of,q as jy,a4 as tm,a6 as Yn,aa as em,a3 as rm,z as Zr,a9 as im,v as nm}from"./index-BwdEWMPj.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
