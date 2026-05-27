dcca2b79b6179e0a5817d2762d16af4e337e33a0
Jzhenli <jzhenl@foxmail.com>
2026-05-27 14:21:59 +0800

    ui

--- apps/xagent/src/xagent/resources/static/assets/index.esm.min-sHegVQzr.js
+++ apps/xagent/src/xagent/resources/static/assets/index.esm.min-sHegVQzr.js
@@ -1,4 +1,4 @@
-import{d as Qy,ag as kf,a1 as Wn,$ as Jy,C as Un,al as Of,q as jy,a5 as tm,a7 as Yn,ab as em,a4 as rm,z as Zr,aa as im,v as nm}from"./index-BT3SAX-i.js";/*! *****************************************************************************
+import{d as Qy,ag as kf,a1 as Wn,$ as Jy,C as Un,al as Of,q as jy,a5 as tm,a7 as Yn,ab as em,a4 as rm,z as Zr,aa as im,v as nm}from"./index-lsWX0aM6.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
